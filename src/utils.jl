b88c1749b7414c15b4af5b52da1baa65fa34e1a5
Miguel Zilhão <mzilhao@gmail.com>
2023-08-07 02:43:22 +0100

    utils [cheb]: allow for generic types

--- src/utils.jl
+++ src/utils.jl
@@ -6,12 +6,12 @@ Return a Chebyshev-Lobatto grid, together with its first and second
 differentiation matrices.
 
 # Arguments
-* `xmin::Real`: rightmost grid point
-* `xmax::Real`: leftmost grid point
-* `N::Integer`: total number of grid points
+* `xmin :: AbstractFloat`: rightmost grid point
+* `xmax :: AbstractFloat`: leftmost grid point
+* `N    :: Integer`: total number of grid points
 """
-function cheb(xmin::T, xmax::T, N::Integer) where {T<:Real}
-    x, D, D2 = cheb(N)
+function cheb(xmin::T, xmax::T, N::Integer) where {T<:AbstractFloat}
+    x, D, D2 = cheb(T,N)
     x = (xmax + xmin .+ (xmax - xmin) * x) / 2
     D  ./= (xmax - xmin) / 2
     D2 ./= (xmax - xmin)^2 / 4
@@ -22,11 +22,11 @@ end
 # taken from Trefethen (2000), "Spectral Methods in MatLab"
 
 "When omitting grid limits, default to [-1,1] interval"
-function cheb(N::Integer)
+function cheb(T::Type, N::Integer)
     @assert(N > 0)
-    x = -cos.(pi*(0:N)/N)
+    x = -cos.(T(pi)*(0:N)/N)
 
-    c  = [2; ones(N-1, 1); 2] .* (-1).^(0:N)
+    c  = [2; ones(T, N-1, 1); 2] .* (-1).^(0:N)
     X  = repeat(x, 1, N+1)
     dX = X - X'
 
@@ -35,3 +35,5 @@ function cheb(N::Integer)
 
     x, D, D*D
 end
+
+cheb(N::Integer) = cheb(Float64, N)
