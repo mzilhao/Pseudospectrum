0382e1ee863a909159f4d6eff6faccc142d3191d
Miguel Zilhão <mzilhao@gmail.com>
2023-08-07 02:31:33 +0100

    test: add some utils tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Pseudospectrum
 using Test
 
+const PS = Pseudospectrum
+
 @testset "Pseudospectrum.jl" begin
-    # Write your tests here.
+    @time @testset "utils tests:" begin include("utils_tests.jl") end
 end
