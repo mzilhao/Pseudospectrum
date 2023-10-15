10fa6c71711584b4cc332ee89f8def693d3e2534
Miguel Zilhão <mzilhao@gmail.com>
2023-10-15 23:50:49 +0100

    tests: update AdS_tests

--- test/AdS_tests.jl
+++ test/AdS_tests.jl
@@ -1,12 +1,13 @@
 using LinearAlgebra
 
-@testset "AdS spectrum test:" begin
+@testset "AdS4 spectrum test:" begin
     N = 64
 
     ℓ = 0
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
-    Op = PS.AdS4_sph(N, ℓ)
-    F  = eigen(Op.L; sortby = x -> abs(real(x)))
+    F  = eigen(Op.A; sortby = x -> abs(real(x)))
     omega  = F.values
 
     # let's compare the N/2 first values, as the error increases for
@@ -21,9 +22,10 @@ using LinearAlgebra
 
 
     ℓ = 2
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
-    Op = PS.AdS4_sph(N, ℓ)
-    F  = eigen(Op.L; sortby = x -> abs(real(x)))
+    F  = eigen(Op.A; sortby = x -> abs(real(x)))
     omega  = F.values
 
     # let's compare the N/2 first values, as the error increases for
@@ -38,11 +40,11 @@ using LinearAlgebra
 
 
     ℓ = 5
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
-    Op = PS.AdS4_sph(N, ℓ)
-    F  = eigen(Op.L; sortby = x -> abs(real(x)))
+    F  = eigen(Op.A; sortby = x -> abs(real(x)))
     omega  = F.values
-
 
     # let's compare the N/2 first values, as the error increases for
     # higher-frequency ones
@@ -59,28 +61,40 @@ end
     N = 12
 
     ℓ = 0
-    G = PS.AdS.build_Gram_matrix_AdS4_sph(Float64, N, ℓ)
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
+
+    G = PS.build_Gram_matrix(Float64, N, model)
     @test G ≈ G'
 
     ℓ = 1
-    G = PS.AdS.build_Gram_matrix_AdS4_sph(Float64, N, ℓ)
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
+
+    G = PS.build_Gram_matrix(Float64, N, model)
     @test G ≈ G'
 
     ℓ = 2
-    G = PS.AdS.build_Gram_matrix_AdS4_sph(Float64, N, ℓ)
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
+
+    G = PS.build_Gram_matrix(Float64, N, model)
     @test G ≈ G'
 
     ℓ = 8
-    G = PS.AdS.build_Gram_matrix_AdS4_sph(Float64, N, ℓ)
-    @test G ≈ G'
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
+    G = PS.build_Gram_matrix(Float64, N, model)
+    @test G ≈ G'
 end
 
-@testset "AdS pseudospectrum test:" begin
+@testset "AdS4 pseudospectrum test:" begin
     N = 64
 
     ℓ = 0
-    Op = PS.AdS4_sph(N, ℓ)
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
     # actual eigenvalues
     x = [2*n + 3 + ℓ for n in 0:10]
@@ -90,7 +104,8 @@ end
 
 
     ℓ = 1
-    Op = PS.AdS4_sph(N, ℓ)
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
     # actual eigenvalues
     x = [2*n + 3 + ℓ for n in 0:10]
@@ -101,12 +116,12 @@ end
 
 
     ℓ = 8
-    Op = PS.AdS4_sph(N, ℓ)
+    model = AdS4_sph(ℓ)
+    Op = Operators(N, model)
 
     # actual eigenvalues
     x = [2*n + 3 + ℓ for n in 0:10]
 
     sigmin = [basic_svd(xi, 0, Op.A) for xi in x]
     @test all(sigmin .< 1e-11)
-
 end
