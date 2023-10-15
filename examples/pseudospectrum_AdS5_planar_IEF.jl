fc739c7ef6ac0db20d6a92fab2b7f48b66fd9474
Miguel Zilhão <mzilhao@gmail.com>
2023-10-15 14:03:42 +0100

    examples: adapt pseudospectrum_AdS5_planar_IEF.jl

--- examples/pseudospectrum_AdS5_planar_IEF.jl
+++ examples/pseudospectrum_AdS5_planar_IEF.jl
@@ -5,22 +5,84 @@ using Plots
 using LaTeXStrings
 
 
+# k2 = 0
+# N  = 48
+
+# Op = AdS5_planar_IEF(N, k2)
+
+
+# Nx = 201
+# Ny = 201
+
+# T = Float64
+
+# xmin = -T(10)
+# xmax =  T(10)
+
+# ymin = -T(12)
+# ymax =  T(0)
+
+# x = range(xmin, xmax, length=Nx)
+# y = range(ymin, ymax, length=Ny)
+
+# sigmin = basic_svd(x, y, Op.A, Op.B)
+
+# # spectra
+# F = eigen(Op.A, Op.B; sortby = x -> real(x)^2 + imag(x)^2)
+# omega = F.values
+
+# #Nmax = Int(floor(N/2))
+# Nmax = 8
+# ω   = omega[1:Nmax]
+# Reω = real(ω)
+# Imω = imag(ω)
+
+
+# #plotlyjs()
+# #pyplot()
+
+# gr()
+
+
+
+# # contourf(x, y, sigmin', color=:plasma, aspect_ratio=:equal,
+# #          levels=12,
+# #          title=L"AdS5 $k^2=0$")
+
+# contourf(x, y, log10.(sigmin'), color=:viridis, aspect_ratio=:equal,
+#          levels=12,
+#          title=L"AdS5 $k^2=0$")
+
+# scatter!(Reω, Imω, shape=:x, color="red", markersize=3, label="QNM")
+
+# xlabel!(L"$\Re(\omega)$")
+# ylabel!(L"$\Im(\omega)$")
+
+# savefig("AdS5_IEF_PS.png")
+
+
+
 k2 = 0
+
+model = AdS5_planar_IEF(k2)
+
+
 N  = 48
 
-Op = AdS5_planar_IEF(N, k2)
+Op = Operators(N, model)
 
 
-Nx = 201
-Ny = 201
+
+Nx = 301
+Ny = 301
 
 T = Float64
 
-xmin = -T(10)
-xmax =  T(10)
+xmin =  T(2.9)
+xmax =  T(3.5)
 
-ymin = -T(12)
-ymax =  T(0)
+ymin = -T(3.0)
+ymax = -T(2.4)
 
 x = range(xmin, xmax, length=Nx)
 y = range(ymin, ymax, length=Ny)
@@ -32,8 +94,8 @@ F = eigen(Op.A, Op.B; sortby = x -> real(x)^2 + imag(x)^2)
 omega = F.values
 
 #Nmax = Int(floor(N/2))
-Nmax = 8
-ω   = omega[1:Nmax]
+#Nmax = 8
+ω   = [omega[1]]
 Reω = real(ω)
 Imω = imag(ω)
 
@@ -50,12 +112,12 @@ gr()
 #          title=L"AdS5 $k^2=0$")
 
 contourf(x, y, log10.(sigmin'), color=:viridis, aspect_ratio=:equal,
-         levels=12,
+         levels=16,
          title=L"AdS5 $k^2=0$")
 
-scatter!(Reω, Imω, shape=:x, color="red", markersize=3, label="QNM")
+scatter!(Reω, Imω, shape=:x, color="red", markersize=3, label="")
 
 xlabel!(L"$\Re(\omega)$")
 ylabel!(L"$\Im(\omega)$")
 
-savefig("AdS5_IEF_PS.png")
+# savefig("AdS5_IEF_PS_zoom.pdf")
