3d452aab430e9d846897b9c9241f46867fe3cafd
Miguel Zilhão <mzilhao@gmail.com>
2023-10-15 20:52:53 +0100

    examples [pseudospectrum_AdS5BH_planar]: use function to plot pseudospectrum

--- examples/pseudospectrum_AdS5BH_planar.jl
+++ examples/pseudospectrum_AdS5BH_planar.jl
@@ -5,119 +5,73 @@ using Plots
 using LaTeXStrings
 
 
-# k2 = 0
-# N  = 48
-
-# Op = AdS5BH_planar(N, k2)
-
-
-# Nx = 201
-# Ny = 201
-
-# T = Float64
-
-# xmin = -T(10)
-# xmax =  T(10)
-
-# ymin = -T(12)
-# ymax =  T(0)
-
-# x = range(xmin, xmax, length=Nx)
-# y = range(ymin, ymax, length=Ny)
-
-# sigmin = basic_svd(x, y, Op.A, Op.B)
-
-# # spectra
-# F = eigen(Op.A, Op.B; sortby = x -> real(x)^2 + imag(x)^2)
-# omega = F.values
-
-# #Nmax = Int(floor(N/2))
-# Nmax = 8
-# ω   = omega[1:Nmax]
-# Reω = real(ω)
-# Imω = imag(ω)
-
-
-# #plotlyjs()
-# #pyplot()
-
-# gr()
-
-
-
-# # contourf(x, y, sigmin', color=:plasma, aspect_ratio=:equal,
-# #          levels=12,
-# #          title=L"AdS5 $k^2=0$")
-
-# contourf(x, y, log10.(sigmin'), color=:viridis, aspect_ratio=:equal,
-#          levels=12,
-#          title=L"AdS5 $k^2=0$")
-
-# scatter!(Reω, Imω, shape=:x, color="red", markersize=3, label="QNM")
-
-# xlabel!(L"$\Re(\omega)$")
-# ylabel!(L"$\Im(\omega)$")
-
-# savefig("AdS5_IEF_PS.png")
-
-
-
 k2 = 0
-
-model = AdS5BH_planar(k2)
-
-
 N  = 48
 
+model = AdS5BH_planar(k2)
 Op = Operators(N, model)
-
-
-
-Nx = 301
-Ny = 301
-
-T = Float64
-
-xmin =  T(2.9)
-xmax =  T(3.5)
-
-ymin = -T(3.0)
-ymax = -T(2.4)
-
-x = range(xmin, xmax, length=Nx)
-y = range(ymin, ymax, length=Ny)
-
-sigmin = basic_svd(x, y, Op.A, Op.B)
 
 # spectra
 F = eigen(Op.A, Op.B; sortby = x -> real(x)^2 + imag(x)^2)
 omega = F.values
 
-#Nmax = Int(floor(N/2))
-#Nmax = 8
+
+function plot_ps(x, y, Op)
+    sigmin = basic_svd(x, y, Op.A, Op.B)
+
+    p = contourf(x, y, log10.(sigmin'), color=:viridis, aspect_ratio=:equal,
+             levels=16,
+             title=L"AdS5 BH $k^2=0$")
+
+    xlabel!(L"$\Re(\omega)$")
+    ylabel!(L"$\Im(\omega)$")
+
+    return p
+end
+
+
+xmin = -10.0
+xmax =  10.0
+ymin = -12.0
+ymax =   0.0
+
+Nx = 201
+Ny = 201
+x  = range(xmin, xmax, length=Nx)
+y  = range(ymin, ymax, length=Ny)
+
+plot_obj = plot_ps(x, y, Op)
+
+# add QNMs
+Nmax = 8
+ω    = omega[1:Nmax]
+Reω  = real(ω)
+Imω  = imag(ω)
+
+scatter!(Reω, Imω, shape=:x, color="red", markersize=3, label="QNM")
+
+savefig("AdS5BH_PS.pdf")
+
+
+
+# zoom in on first mode
+xmin =  2.9
+xmax =  3.5
+ymin = -3.0
+ymax = -2.4
+
+Nx = 301
+Ny = 301
+x = range(xmin, xmax, length=Nx)
+y = range(ymin, ymax, length=Ny)
+
+plot_obj = plot_ps(x, y, Op)
+
+# add QNM
 ω   = [omega[1]]
 Reω = real(ω)
 Imω = imag(ω)
 
-
-#plotlyjs()
-#pyplot()
-
-gr()
-
-
-
-# contourf(x, y, sigmin', color=:plasma, aspect_ratio=:equal,
-#          levels=12,
-#          title=L"AdS5 $k^2=0$")
-
-contourf(x, y, log10.(sigmin'), color=:viridis, aspect_ratio=:equal,
-         levels=16,
-         title=L"AdS5 $k^2=0$")
-
 scatter!(Reω, Imω, shape=:x, color="red", markersize=3, label="")
 
-xlabel!(L"$\Re(\omega)$")
-ylabel!(L"$\Im(\omega)$")
-
-# savefig("AdS5_IEF_PS_zoom.pdf")
+savefig("AdS5BH_PS_zoom.pdf")
